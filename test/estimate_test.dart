504ffa53d7667a0d33e84b1ed23cf7f6df3d40d7
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-08-17 23:47:33 -0700

    Estimate: Stub class.

---
+++ test/estimate_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+import 'package:hose_jockey/models/estimate.dart';
+
+void main() {
+  test('Estimate value should be incremented', () {
+    final estimate = Estimate();
+
+  });
+}
