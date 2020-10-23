038b1a9138b102c05efb279937304ea92fc2a602
Crowley <jcrowley317@gmail.com>
2020-10-23 11:22:42 -0700

    Implemented selecting fire leads to results
    resolve #6

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'new_estimate.dart';
+import 'results_screen.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -12,103 +13,25 @@ class MainScreen extends StatelessWidget {
         child: ListView(
           padding: const EdgeInsets.all(8),
           children: <Widget>[
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 1')),
+            GestureDetector (
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NewResultsScreen()),
+                );
+              },
+              child: Container(
+                height: 50,
+                color: Colors.blue[300],
+                child: const Center(child: Text('Entry A')),
+              ),
+  
             ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 2')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 3')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 4')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 5')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 6')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 7')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 8')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 9')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 10')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 11')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 12')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 13')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 14')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 15')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 16')),
-            ),
+            
           ],
         ),
       ),
         
-      
-      // body: Center(
-      //   child: ElevatedButton(
-      //     child: Text('Estimate'),
-      //     onPressed: (){
-      //       // Navigate to other page
-      //       Navigator.push(
-      //         context,
-      //         MaterialPageRoute(builder: (context) => NewEstimateScreen()),
-      //       );
-      //     },
-      //   ),
-      // ),
       floatingActionButton: FloatingActionButton(	
         onPressed: () {
           Navigator.push(
