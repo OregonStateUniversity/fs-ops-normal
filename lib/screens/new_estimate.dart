ca3c45b3ea5d521e6c72003477e06cd66899d05c
Kyle Hannon <khannon@cocc.edu>
2020-10-28 20:26:13 -0700

    Added state to new_estimate screen

--- lib/screens/new_estimate.dart
+++ lib/screens/new_estimate.dart
@@ -1,9 +1,55 @@
 import 'package:flutter/material.dart';
 import 'results_screen.dart';
 
-class NewEstimateScreen extends StatelessWidget {
+//class NewEstimateScreen extends StatelessWidget {
+//  @override
+// Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text('New Estimate'),
+//      ),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            Text('Enter Acreage',
+//            ),
+//            TextField(
+//              keyboardType: TextInputType.number,
+//              decoration: InputDecoration(
+//                hintText: "Acreage"
+//              ),
+//            ),
+//            OutlineButton(
+//                onPressed: (){
+//                  // Show results when clicked
+//                  Navigator.push(
+//                    context,
+//                    MaterialPageRoute(builder: (context) => NewResultsScreen()),
+//                  );
+//                },
+//                child: Text('Calculate Estimate'),
+//            )
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
+
+
+
+class NewEstimateScreen extends StatefulWidget{
+
+  _NewEstimateScreenState createState() => _NewEstimateScreenState();
+}
+
+class _NewEstimateScreenState extends State<NewEstimateScreen> {
+  var acreage;
+
+
   @override
- Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('New Estimate'),
@@ -14,26 +60,21 @@ class NewEstimateScreen extends StatelessWidget {
           children: <Widget>[
             Text('Enter Acreage',
             ),
-            TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return null;
-                }
-                return value;
-              },
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  hintText: "Acreage"
+              ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric( vertical: 16.0),
-              child: OutlineButton(
-                onPressed: (){
-                  // Show results when clicked
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NewResultsScreen()),
-                  );
-                },
-                child: Text('Calculate Estimate'),
-              )
+            OutlineButton(
+              onPressed: (){
+                // Show results when clicked
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NewResultsScreen()),
+                );
+              },
+              child: Text('Calculate Estimate'),
             )
           ],
         ),
