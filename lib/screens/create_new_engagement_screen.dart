568059e652209b764914445106b848c591715d10
Crowley <jcrowley317@gmail.com>
2020-11-03 19:11:16 -0800

    Create create_new_engagement_screen.dart
    created bare page for creating a new engagement.

---
+++ lib/screens/create_new_engagement_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class CreateNewEngagement extends StatefulWidget{
+
+  _CreateNewEngagementState createState() => _CreateNewEngagementState();
+
+}
+
+class _CreateNewEngagementState extends State<CreateNewEngagement> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    // each new engagement created should have a name and a bool set to active
+    throw UnimplementedError();
+  }
+}
