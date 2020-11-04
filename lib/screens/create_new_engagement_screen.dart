dc629746578d6f88507302eb4bde349defa820cb
Crowley <jcrowley317@gmail.com>
2020-11-03 19:17:17 -0800

    Update create_new_engagement_screen.dart

--- lib/screens/create_new_engagement_screen.dart
+++ lib/screens/create_new_engagement_screen.dart
@@ -11,8 +11,10 @@ class _CreateNewEngagementState extends State<CreateNewEngagement> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    // each new engagement created should have a name and a bool set to active
+    // TODO: 
+    //       -implement form fields
+    //       -data variables
+    //       -each new engagement created should have a name and a bool set to active
     throw UnimplementedError();
   }
 }
