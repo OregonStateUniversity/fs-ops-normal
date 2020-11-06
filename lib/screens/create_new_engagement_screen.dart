3612d5fe41809593d48afcc48b8e17fc14be94c2
Crowley <jcrowley317@gmail.com>
2020-11-05 17:26:03 -0800

    Update create_new_engagement_screen.dart
    added text fields for name and initial acreage

--- lib/screens/create_new_engagement_screen.dart
+++ lib/screens/create_new_engagement_screen.dart
@@ -9,12 +9,49 @@ class CreateNewEngagement extends StatefulWidget{
 
 class _CreateNewEngagementState extends State<CreateNewEngagement> {
 
+  Widget _buildNameField() {
+    return const TextField(
+      autofocus: true,
+      textCapitalization: TextCapitalization.words,
+      maxLines: 1,
+      decoration: InputDecoration(
+        labelText: 'Enter fire name:',
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+
+  bool _acreageInputIsValid = true;
+
+  Widget _buildAcreageField() {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: 'Enter acreage:',
+        errorText:_acreageInputIsValid ? null: 'Please enter a number',
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: 
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(8.0),
+        children: <Widget>[
+          const ListTile(title: Text('Name Fire')),
+          _buildNameField(),
+          const ListTile(title: Text('Set initial acreage')),
+          _buildAcreageField(),
+        ],
+      )
+    );
+  }
+}
+
+// TODO: 
     //       -implement form fields
     //       -data variables
     //       -each new engagement created should have a name and a bool set to active
-    throw UnimplementedError();
-  }
-}
+    //throw UnimplementedError();
