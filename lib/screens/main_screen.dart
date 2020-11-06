8e9c631ab3f48f6ba10d8df2a64d0209b2bcf508
Crowley <jcrowley317@gmail.com>
2020-11-05 22:11:25 -0800

    Update main_screen.dart
    fires linked to selected engagement page

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hose_jockey/screens/selected_engagement.dart';
 import 'create_new_engagement_screen.dart';
 //import 'results_screen.dart';
 
@@ -42,17 +43,63 @@ class _MainScreenState extends State<MainScreen> {
         ),
         body: Scrollbar(
           child: ListView.builder(
-            padding: const EdgeInsets.all(8),
+
             itemCount: fires.length,
             itemBuilder: (context, index){
-              return ListTile(
-                title: Text('${fires[index]}'),
-                );
+              //return Padding(
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SelectedEngagement()),
+                  );
+                },
+                child: ListTile(
+                  title: Text('${fires[index].name}'),
+                  subtitle: Text('Current Size: ${fires[index].size}'),
+                ),
+                // padding: const EdgeInsets.all(8),
+                // child: Card(
+                //   elevation: 5.0,
+                //   color: Colors.blue[100],
+                //   shape: RoundedRectangleBorder(
+                //     borderRadius: BorderRadius.circular(5.0)
+                //   ),
+                  
+                  //margin: EdgeInsets.fromLTRB(2, 1, 2, 0),
+                  // child: Container(
+                  //   child: ListView.builder(
+                  //     //padding: EdgeInsets.only(left: 8),
+                  //     itemCount: fires.length,
+                  //     itemBuilder: (BuildContext context, index) {
+                  //       return GestureDetector(
+                  //         onTap: () {
+                  //           Navigator.push(
+                  //             context,
+                  //             MaterialPageRoute(builder: (context) => SelectedEngagement()),
+                  //           );
+                  //         },
+                  //         child: ListTile(
+                  //           title: Text('${fires[index].name}'),
+                  //           subtitle: Text('${fires[index].size}'),
+                  //         )
+                  //       );
+                  //     }
+                  //   )
+                  // ),
+                //),
+              );
             },
           )
         ),
         
 
+            // onTap: () {
+            //   Navigator.push(
+            //     context,
+            //     MaterialPageRoute(builder: (context) => SelectedEngagement()),
+            //   );
+            // },
         floatingActionButton: FloatingActionButton(	
           onPressed: () {
             Navigator.push(
