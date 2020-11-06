6bf95add3820a6d8aeca0b78b30482f136e22934
Crowley <jcrowley317@gmail.com>
2020-11-05 17:27:13 -0800

    Update main_screen.dart
    list now builts from list of fires
    created Fire class

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,120 +1,68 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'new_estimate.dart';
-import 'results_screen.dart';
+import 'create_new_engagement_screen.dart';
+//import 'results_screen.dart';
 
-class MainScreen extends StatelessWidget {
+class Fire{
+  String name;
+  int size;
+
+  Fire(this.name, this.size);
+
   @override
- Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ops Normal'),
-      ),
-      body: Scrollbar(
-        child: ListView(
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
-            GestureDetector (
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => NewResultsScreen( acreage: '0',)),
-                );
-              },
-              child: Container(
-                height: 50,
-                color: Colors.blue[300],
-                child: const Center(child: Text('Fire 1')),
-              ),
-  
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 2')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 3')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 4')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 5')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 6')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 7')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 8')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 9')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 10')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 11')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 12')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 13')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 14')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[300],
-              child: const Center(child: Text('Fire 15')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.blue[100],
-              child: const Center(child: Text('Fire 16')),
-            ),
-          ],
+  String toString() {
+    return '{ ${this.name}, ${this.size} }';
+  }
+}
+
+class MainScreen extends StatefulWidget{
+  const MainScreen({Key key}) : super(key: key);
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List fires = [
+    Fire('Alpha Fire', 500),
+    Fire('Beta Fire', 1000),
+    Fire('Gamma Fire', 575)
+  ];
+
+  // _MainScreenState({Key key, @required this.fires}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Ops Normal';    
+    
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
         ),
-      ),
+        body: Scrollbar(
+          child: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: fires.length,
+            itemBuilder: (context, index){
+              return ListTile(
+                title: Text('${fires[index]}'),
+                );
+            },
+          )
+        ),
         
-      floatingActionButton: FloatingActionButton(	
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NewEstimateScreen()),
-          );
-        },	
-        tooltip: 'New estimate',	
-        child: Icon(Icons.add),	
+
+        floatingActionButton: FloatingActionButton(	
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CreateNewEngagement()),
+            );
+          },	
+          tooltip: 'New estimate',	
+          child: Icon(Icons.add),	
+        )
       )
     );
   }
