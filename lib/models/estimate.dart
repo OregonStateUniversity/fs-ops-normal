c6ab555b9b76d8cd09a8c755fb01e56fdb6bcd4a
Kyle Hannon <khannon@cocc.edu>
2020-11-18 18:08:18 -0800

    Consolidated estimate and order models into one.

--- lib/models/estimate.dart
+++ lib/models/estimate.dart
@@ -2,9 +2,11 @@ class Estimate {
   static const BASE_TRUNK_LINE_LENGTH = 1000;
   static const TRUNK_LINE_LENGTH_PER_ACRE = 200;
 
+  final String name;
+  final String timestamp;
   final int acres;
 
-  Estimate({this.acres});
+  Estimate({this.name, this.acres, this.timestamp});
 
   int get trunkLineLength => BASE_TRUNK_LINE_LENGTH + TRUNK_LINE_LENGTH_PER_ACRE * acres;
 
