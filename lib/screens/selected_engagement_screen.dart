097020c6e83d1813708085751a2e0efb1ec5d0a2
kylehannon-cs <krhannon7333@gmail.com>
2020-11-22 16:33:41 -0800

    Orders are now listed from newest to oldest

--- lib/screens/selected_engagement_screen.dart
+++ lib/screens/selected_engagement_screen.dart
@@ -23,7 +23,8 @@ class _SelectedEngagementState extends State<SelectedEngagement> {
 
   @override
   Widget build(BuildContext context) {
-    if(widget.orders.isEmpty){
+    List<Estimate> orders = widget.orders.reversed.toList();
+    if(orders.isEmpty){
       return Scaffold(
         appBar: AppBar(
           title: Text("${widget.name}"),
@@ -58,16 +59,16 @@ class _SelectedEngagementState extends State<SelectedEngagement> {
       body: Scrollbar(
         child: ListView.builder(
           padding: const EdgeInsets.all(8),
-          itemCount: widget.orders.length,
+          itemCount: orders.length,
           itemBuilder: (context, index){
             return ListTile(
-              title: Text('Order ${widget.orders.length - index}'), // TODO: Replace this name with actual name later
-              subtitle: Text('${widget.orders[index].acres.toString()} Acres\nCreate on: ${widget.orders[index].timeStamp}\n'),
+              title: Text('Order ${orders.length - index}'), // TODO: Replace this name with actual name later
+              subtitle: Text('${orders[index].acres.toString()} Acres\nCreate on: ${orders[index].timeStamp}\n'),
               onTap: () {
                 // TODO: show saved item from here this is temporary
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => FinalizedOrderScreen(widget.orders[index]),)
+                  MaterialPageRoute(builder: (context) => FinalizedOrderScreen(orders[index]),)
                 );
               },
             );
