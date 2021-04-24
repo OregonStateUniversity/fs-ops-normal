449bda4479df0926439a099db59b0ada9be8b670
kylehannon-cs <krhannon7333@gmail.com>
2021-04-24 16:52:36 -0700

    Changed pdf viewer to a more robust one

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,22 @@ import 'package:hose_jockey/app.dart';
 import 'package:hose_jockey/screens/main_screen.dart';
 
 void main() {
-  testWidgets('Main Screen is empty', (WidgetTester tester) async {
 
-    await tester.pumpWidget(MainScreen());
+  Widget createWidgetForTesting({Widget child}){
+    return MaterialApp(
+      home: child,
+    );
+  }
+
+  testWidgets('main_screen empty', (WidgetTester tester) async {
+
+    await tester.pumpWidget(createWidgetForTesting(child: new MainScreen()));
 
     final titleFinder = find.text('Ops Normal');
     final messageFinder = find.text('No Engagements Created Yet');
+    expect(titleFinder, findsOneWidget);
+    expect(messageFinder, findsOneWidget);
+    await tester.pumpAndSettle();
+
   });
 }
