fa321b1c785050e0936e0ac105746e643bdd2a01
kylehannon-cs <krhannon7333@gmail.com>
2021-04-23 18:02:42 -0700

    Remove scale package that won't be used any more

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,6 @@ import 'screens/modify_estimate_screen.dart';
 import 'screens/engagement_screen.dart';
 import 'screens/about_screen.dart';
 import 'screens/how_to_screen.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 
 class App extends StatelessWidget {
 
@@ -25,17 +24,6 @@ class App extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (context, widget) => ResponsiveWrapper.builder(
-        BouncingScrollWrapper.builder(context, widget),
-        maxWidth: 1200,
-        minWidth: 450,
-        defaultScale: true,
-        breakpoints: [
-          ResponsiveBreakpoint.autoScale(450, name: MOBILE),
-          ResponsiveBreakpoint.autoScale(650, name: TABLET),
-          ResponsiveBreakpoint.autoScale(1000, name: TABLET),
-        ]
-      ),
       initialRoute: '/',
       title: 'Ops Normal',
       debugShowCheckedModeBanner: false,
