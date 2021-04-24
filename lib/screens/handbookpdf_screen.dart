5598bc2f49045a16b6b62903a2255189e462349c
kylehannon-cs <krhannon7333@gmail.com>
2021-04-24 16:52:36 -0700

    Changed pdf viewer to a more robust one

--- lib/screens/handbookpdf_screen.dart
+++ lib/screens/handbookpdf_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:native_pdf_view/native_pdf_view.dart';
+
 
 class HandBookPdf extends StatelessWidget{
   static const routeName = 'handbook';
   static const title = "Hand Book";
 
+  final pdfController = PdfController(document: PdfDocument.openAsset('assets/redbook.pdf'));
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Container(
-
-      )
+      body: pdfView()
     );
   }
+  Widget pdfView() => PdfView(
+    controller: pdfController,
+  );
 }
