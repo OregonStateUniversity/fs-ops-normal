9eac7483238924865695e320738bf9ba11097f1a
kylehannon-cs <krhannon7333@gmail.com>
2021-04-24 17:38:06 -0700

    Added reusable bottom bar

--- lib/screens/handbookpdf_screen.dart
+++ lib/screens/handbookpdf_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
-
+import '../Widgets/ru_bottom_nav_bar.dart';
 
 class HandBookPdf extends StatelessWidget{
   static const routeName = 'handbook';
-  static const title = "Hand Book";
+  static const title = "Red Book PDF";
 
   final pdfController = PdfController(document: PdfDocument.openAsset('lib/assets/RedBook.pdf'));
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: pdfView()
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: pdfView(),
+      bottomNavigationBar: RU_BottomNavBar(goBack: '/',),
     );
   }
   Widget pdfView() => PdfView(
