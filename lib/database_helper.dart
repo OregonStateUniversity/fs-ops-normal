50a76bf59d60c0451ff2d65c6e8eb86c5fba495a
Ian Snyder <isnide23@gmail.com>
2022-01-20 10:47:37 -0800

    fix typeing in Database Helper References #55

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -22,14 +22,14 @@ class DatabaseHelper {
           'CREATE TABLE IF NOT EXISTS engagements(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, timeStamp TEXT NOT NULL, acres INTEGER NOT NULL, active INTEGER NOT NULL, orders TEXT NOT NULL);');
     });
 
-    return _database;
+    return _database!;
   }
 
 // This block below is causing a runtime error
 
   static Future<List<Map>> getAllEngagements() async {
-    final Database db = await (getDBConnector() as FutureOr<Database>);
-    var records = await db.rawQuery('SELECT * FROM engagements');
+    final Database? db = await getDBConnector();
+    var records = await db!.rawQuery('SELECT * FROM engagements');
     return records;
   }
 
@@ -39,9 +39,9 @@ class DatabaseHelper {
   }
 
   static Future<void> insertEngagement(dto) async {
-    final Database db = await (getDBConnector() as FutureOr<Database>);
+    final Database? db = await getDBConnector();
 
-    await db.transaction((txn) async {
+    await db!.transaction((txn) async {
       await txn.rawInsert(
           'INSERT INTO engagements(name, timeStamp, acres, active, orders) VALUES(?, ?, ?, ?, ?)',
           [dto.name, dto.timeStamp, dto.size, 1, "[]"]);
@@ -49,35 +49,35 @@ class DatabaseHelper {
   }
 
   static Future<void> deleteEngagement(index) async {
-    final Database db = await (getDBConnector() as FutureOr<Database>);
+    final Database? db = await getDBConnector();
 
-    await db.transaction((txn) async {
+    await db!.transaction((txn) async {
       await txn.rawDelete('DELETE FROM engagements WHERE id = $index');
     });
   }
 
   static Future<void> archiveEngagement(index) async {
-    final Database db = await (getDBConnector() as FutureOr<Database>);
+    final Database? db = await getDBConnector();
 
-    await db.transaction((txn) async {
+    await db!.transaction((txn) async {
       await txn
           .rawUpdate('UPDATE engagements SET active = 0 WHERE id = $index');
     });
   }
 
   static Future<void> unarchiveEngagement(index) async {
-    final Database db = await (getDBConnector() as FutureOr<Database>);
+    final Database? db = await getDBConnector();;
 
-    await db.transaction((txn) async {
+    await db!.transaction((txn) async {
       await txn
           .rawUpdate('UPDATE engagements SET active = 1 WHERE id = $index');
     });
   }
 
   static Future<void> insertOrder(eng, order) async {
-    final Database db = await (getDBConnector() as FutureOr<Database>);
+    final Database? db = await getDBConnector();
     eng.orders.insert(0, order);
-    await db.transaction((txn) async {
+    await db!.transaction((txn) async {
       String tmp = "'[";
       eng.orders.forEach((value) {
         tmp += json.encode(value.toJson());
@@ -93,12 +93,12 @@ class DatabaseHelper {
   }
 
   static Future<void> deleteOrder(eng, order) async {
-    final Database db = await (getDBConnector() as FutureOr<Database>);
+    final Database? db = await getDBConnector();
 
     eng.orders.toList().remove(order);
     var newOrderList = eng.orders;
 
-    await db.transaction((txn) async {
+    await db!.transaction((txn) async {
       String tmp = "'[";
       newOrderList.forEach((value) {
         tmp += json.encode(value.toJson());
