b931b8c0d4f33ae86b96e39ec78dada3575aa69a
Ian Snyder <isnide23@gmail.com>
2022-02-01 13:37:15 -0800

    gut alert dialog in engagement screen

--- lib/screens/engagement_screen.dart
+++ lib/screens/engagement_screen.dart
@@ -218,78 +218,12 @@ class _SelectedEngagementState extends State<SelectedEngagement> {
                     controller: acreageCon,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
-                      labelText: 'Structures',
-                      border: const OutlineInputBorder(),
-                      errorText: _validate ? 'Value Can\'t Be Empty' : null,
-                      hintText: 'Structures',
-                    ),
-                  ),
-                  TextField(
-                    autofocus: true,
-                    controller: acreageCon,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
                       labelText: 'Enter Acreage',
                       border: const OutlineInputBorder(),
                       errorText: _validate ? 'Value Can\'t Be Empty' : null,
                       hintText: 'Acreage',
                     ),
                   ),
-                  DropdownButton<String>(
-                    value: dropdownValue,
-                    icon: const Icon(Icons.arrow_downward),
-                    iconSize: 24,
-                    elevation: 16,
-                    style: const TextStyle(color: Colors.deepPurple),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.deepPurpleAccent,
-                    ),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownValue = "Defualt";
-                      });
-                    },
-                    items: <String>['Timber', 'Grass']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                  DropdownButton<String>(
-                    value: dropdownValue2,
-                    icon: const Icon(Icons.arrow_downward),
-                    iconSize: 24,
-                    elevation: 16,
-                    style: const TextStyle(color: Colors.deepPurple),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.deepPurpleAccent,
-                    ),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        dropdownValue = newValue;
-                      });
-                    },
-                    items: <String>['Shape', 'ShapeTwo']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                  DropdownButton<String>(
-                    items: <String>['Grass', 'Timber'].map((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    onChanged: (_) {},
-                  )
                 ],
               ),
               actions: <Widget>[
