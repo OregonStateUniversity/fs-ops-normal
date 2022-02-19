f01aea443a30cf93620991265c327f5c9b721a7f
Yong Bakos <ybakos@humanoriented.com>
2022-02-19 14:20:26 -0800

    Util: Format how to instructions strings.
    The class definitions don't follow proper Dart conventions. Reformat.

--- lib/utils/how_to_instructions_strings.dart
+++ lib/utils/how_to_instructions_strings.dart
@@ -1,6 +1,7 @@
-class HowToCreateEngagementAndOrder{
+class HowToCreateEngagementAndOrder {
   static const title = "Creating Your First Engagement and Order";
-  static const description = "1. On the home page click the bottom plus(+) button to create a new engagement. And engagement is a fire that you are currently working.\n"
+  static const description =
+      "1. On the home page click the bottom plus(+) button to create a new engagement. And engagement is a fire that you are currently working.\n"
       "\t\t\ta. Give the engagement a name, something to describe the fire like location\n"
       "\t\t\tb. Click the 'Create' button\n"
       "\t\t\tc. You should see a new engagement show up on the screen with the name you've just entered."
@@ -15,30 +16,33 @@ class HowToCreateEngagementAndOrder{
       "\t\t\tb. To go back and create another engagement tap the 'Home' button.";
 }
 
-class HowToDeleteEngagementOrOrder{
+class HowToDeleteEngagementOrOrder {
   static const title = "How to Delete an Engagement or Order";
-  static const description = "1. Find the engagement or order that you want to delete.\n"
+  static const description =
+      "1. Find the engagement or order that you want to delete.\n"
       "\t\t\ta. Tap and hold the left side of the screen on the engagement or order and slide it to the right.\n"
       "\t\t\tb. A popup will ask you to confirm the deletion. Tap on the 'Delete' button. Warning: This cannot be undone.";
 }
 
-class HowToArchiveEngagement{
+class HowToArchiveEngagement {
   static const title = "How to Archive an Engagement";
-  static const description = "1. On the main page find the engagement that you wish to archive.\n"
+  static const description =
+      "1. On the main page find the engagement that you wish to archive.\n"
       "\t\t\ta. Tap and hold the left side of the screen on the engagement and slide to the right.\n"
       "\t\t\tb. A popup will ask you to confirm the archive. Tap on the 'Yes' button to confirm.\n";
 }
 
-class HowToUnarchiveEngagement{
+class HowToUnarchiveEngagement {
   static const title = "How to Unarchive an Engagement";
-  static const description = "1. First you need to get to the archive page by tapping on the 'Archive' button in the bottom left corner.\n"
+  static const description =
+      "1. First you need to get to the archive page by tapping on the 'Archive' button in the bottom left corner.\n"
       "\t\t\ta. Once you are on this page you now need to locate the engagement that you want to un archive.\n"
       "\t\t\tb. Once located you need to tap and hold the left side of the screen on the engagement and slide it to the right.\n"
       "\t\t\tc. A popup will ask you to confirm the unarchive. Tap on the 'Yes' button to confirm.\n";
 }
 
-class HowToChangeVisualSettings{
+class HowToChangeVisualSettings {
   static const title = "How to Change Visual Settings";
-  static const description = "To change the view of the application tap on the 'Visual Settings' button inside the left hand slide out drawer ";
+  static const description =
+      "To change the view of the application tap on the 'Visual Settings' button inside the left hand slide out drawer ";
 }
-
