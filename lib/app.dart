38cec3f35e5e804c67acff8bdfdf03ab55a9cc45
Yong Bakos <ybakos@humanoriented.com>
2022-02-19 08:33:01 -0800

    App: Alphabetize imports.

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'screens/main_screen.dart';
-import 'screens/new_estimate_screen.dart';
-import 'screens/new_engagement_screen.dart';
-import 'screens/estimate_screen.dart';
-import 'screens/modify_estimate_screen.dart';
-import 'screens/engagement_screen.dart';
+
 import 'screens/about_screen.dart';
-import 'screens/how_to_screen.dart';
 import 'screens/create_order_screen.dart';
+import 'screens/engagement_screen.dart';
+import 'screens/estimate_screen.dart';
+import 'screens/how_to_screen.dart';
+import 'screens/main_screen.dart';
+import 'screens/modify_estimate_screen.dart';
+import 'screens/new_engagement_screen.dart';
+import 'screens/new_estimate_screen.dart';
 
 class App extends StatelessWidget {
   static final routes = {
