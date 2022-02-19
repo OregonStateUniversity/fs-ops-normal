b2eb6d00e5de17548a1bc38a0fd37a267a45888a
Yong Bakos <ybakos@humanoriented.com>
2022-02-19 08:28:08 -0800

    App: remove unused screen, uncomment NewEstimateScreen route, cleanup routes.
    There is an unused imported screen that has been there since fall 2022 start.
    Remove it.
    
    Uncomment the new estimate screen route. Seems needed. Not sure why this is being removed.
    
    Use the MainScreen.routeName for the root route of App's build method.

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hose_jockey/screens/handbookpdf_screen.dart';
 import 'screens/main_screen.dart';
 import 'screens/new_estimate_screen.dart';
 import 'screens/new_engagement_screen.dart';
@@ -13,21 +12,20 @@ import 'screens/create_order_screen.dart';
 class App extends StatelessWidget {
   static final routes = {
     MainScreen.routeName: (context) => MainScreen(),
-    SelectedEngagement.routeName: (BuildContext context) =>
-        SelectedEngagement(),
+    AboutScreen.routeName: (context) => AboutScreen(),
     CreateNewEngagement.routeName: (context) => CreateNewEngagement(),
     CreateOrderScreen.routeName: (context) => CreateOrderScreen(),
-    //NewEstimateScreen.routeName: (context) => NewEstimateScreen(),
     EstimateScreen.routeName: (context) => EstimateScreen(),
-    ModifyEstimateScreen.routeName: (context) => ModifyEstimateScreen(),
-    AboutScreen.routeName: (context) => AboutScreen(),
     HowTo.routeName: (context) => HowTo(),
+    ModifyEstimateScreen.routeName: (context) => ModifyEstimateScreen(),
+    NewEstimateScreen.routeName: (context) => NewEstimateScreen(),
+    SelectedEngagement.routeName: (context) => SelectedEngagement(),
   };
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: MainScreen.routeName,
       title: 'Ops Normal',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
