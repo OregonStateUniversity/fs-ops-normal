8974a4743354ce40a960006952a13ee5cc9524c4
Yong Bakos <ybakos@humanoriented.com>
2022-02-19 22:07:46 -0800

    BottomNavBar: Alphabetize imports.

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,6 +1,6 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
-import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
 import 'bottom_icon.dart';
 
 class BottomNavBar extends StatefulWidget {
