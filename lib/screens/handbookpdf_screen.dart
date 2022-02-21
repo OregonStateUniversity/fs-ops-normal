5c4349ffc396eb40789a2a0b2bac00d7bab45b73
OconnellErin <oconerin@oregonstate.edu>
2022-02-21 14:51:10 -0800

    resolve issue #94, made all variables final

--- lib/screens/handbookpdf_screen.dart
+++ lib/screens/handbookpdf_screen.dart
@@ -3,9 +3,9 @@ import 'package:native_pdf_view/native_pdf_view.dart';
 import '../widgets/bottom_nav_bar.dart';
 
 class OpenPdfRedBook extends StatelessWidget {
-  static const title = "Red Book";
+  static final title = "Red Book";
 
-  var pdfController =
+  final pdfController =
       PdfController(document: PdfDocument.openAsset('lib/assets/RedBook.pdf'));
   final pageController = TextEditingController();
 
