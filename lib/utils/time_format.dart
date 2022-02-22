30786dde030e875cb55d63b18a1ce524edfee08a
OconnellErin <oconerin@oregonstate.edu>
2022-02-21 23:12:52 -0800

    resolve issue #83, extracted format string into const FULL_DATE_AND_TIME_FORMAT

--- lib/utils/time_format.dart
+++ lib/utils/time_format.dart
@@ -1,7 +1,9 @@
 import 'package:intl/intl.dart';
 
+const FULL_DATE_AND_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
+
 class TimeFormat {
-  static var format = new DateFormat("dd/MM/yyyy HH:mm:ss");
+  static var format = new DateFormat(FULL_DATE_AND_TIME_FORMAT);
 
   static get currentTime {
     return DateFormat.jm().format(DateTime.now());
