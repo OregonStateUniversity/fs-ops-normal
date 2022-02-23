bc39985c28f3935200fbdc71f094e6980030c106
OconnellErin <oconerin@oregonstate.edu>
2022-02-23 15:42:11 -0800

    Time_Format.dart: Updated to arrow function syntax, resolved issue #84

--- lib/utils/time_format.dart
+++ lib/utils/time_format.dart
@@ -5,7 +5,5 @@ const FULL_DATE_AND_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
 class TimeFormat {
   static var format = new DateFormat(FULL_DATE_AND_TIME_FORMAT);
 
-  static get currentTime {
-    return format.format(DateTime.now());
-  }
+  static get currentTime => format.format(DateTime.now());
 }
