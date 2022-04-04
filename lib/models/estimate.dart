ffefa81c5b582ef104839f050bc8a348594e0a21
OconnellErin <oconerin@oregonstate.edu>
2022-04-04 12:50:44 -0700

    Estimate.dart: Made toStringStructures call calculateSprinklers()

--- lib/models/estimate.dart
+++ lib/models/estimate.dart
@@ -198,25 +198,7 @@ class Estimate {
   }
 
   String toCopyStringStructures() {
-    String str = "Trunk Line: ${this.trunkLineLength} ft.\n"
-        "Lat Line: ${this.latLineLength} ft.\n"
-        "Toy Hose: ${this.toyLineLength} ft.\n\n";
-
-    str += "1.5\" Gated Wye: ${this.fittings}\n"
-        "1.5\" Reducers: ${this.fittings}\n"
-        "1\"-3/4\" Reducers: ${this.fittings}\n"
-        "Forester Nozzles: ${this.fittings}\n"
-        "Toy Nozzles: ${this.fittings}\n"
-        "Toy Wye: ${this.fittings}\n\n";
-    str += "Folda-tank: ${this.calculateFoldaTanks()}\n"
-        "Mark 3 + Kits: ${this.calculateMark3s()}\n"
-        "Pump Mix (Cans): ${this.calculatePumpMixCans()}\n\n";
-
-    str += "Water (Pallets): ${this.calculateWaterPallets()}\n"
-        "Gatorade (Pallets): ${this.calculateGatoradePallets()}\n"
-        "MRE (Pallets): ${this.calculateMREPallets()}\n"
-        "Port-a-Potties: ${this.calculatePortaPotties()}\n\n";
-
+    String str = "Sprinkler Kits: ${calculateSprinklers()}\n";
     return str;
   }
 
