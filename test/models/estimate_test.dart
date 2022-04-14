890ac22e5ae35dfe4e3fd80edaaaf1ed4854a99e
Ian Snyder <isnide23@gmail.com>
2022-04-14 16:38:08 -0700

    unit tests passing, toFlatFireText output extended to include all equipment calcualtions References #71

--- test/models/estimate_test.dart
+++ test/models/estimate_test.dart
@@ -24,8 +24,7 @@ void main() {
       Estimate estimate;
 
       //Act
-      estimate = new Estimate.jsonConvF(
-          1, 1, 2, "time", 1200, 600, 300, 6);
+      estimate = new Estimate.jsonConvF(1, 1, 2, "time", 1200, 600, 300, 6);
 
       //Assert
       expect(estimate.name, equals(1));
@@ -71,8 +70,7 @@ void main() {
 
     test('has initial calculations', () {
       //Arrange
-      Estimate estimate = new Estimate();
-      estimate.acres = 1;
+      Estimate estimate = new Estimate(acres: 1);
 
       //Assert
       expect(estimate.acres, equals(1));
@@ -84,8 +82,7 @@ void main() {
 
     test('toCopyString that is 1-9 acres', () {
       //Arrange
-      Estimate estimate = new Estimate();
-      estimate.acres = 1;
+      Estimate estimate = new Estimate(acres: 1);
       var expected = "Trunk Line: 1200 ft.\n"
           "Lat Line: 600 ft.\n"
           "Toy Hose: 300 ft.\n\n"
@@ -94,7 +91,14 @@ void main() {
           "1\"-3/4\" Reducers: 6\n"
           "Forester Nozzles: 6\n"
           "Toy Nozzles: 6\n"
-          "Toy Wye: 6\n\n";
+          "Toy Wye: 6\n\n"
+          "Folda-tank: 0\n"
+          "Mark 3 + Kits: 0\n"
+          "Pump Mix (Cans): 0\n\n"
+          "Water (Pallets): 0\n"
+          "Gatorade (Pallets): 0\n"
+          "MRE (Pallets): 0\n"
+          "Port-a-Potties: 0\n\n";
 
       //Act
       var actual = estimate.flatFireOrderText();
@@ -105,8 +109,7 @@ void main() {
 
     test('toCopyString that is 10-19 acres', () {
       //Arrange
-      Estimate estimate = new Estimate();
-      estimate.acres = 10;
+      Estimate estimate = new Estimate(acres: 10);
       var expected = "Trunk Line: 3000 ft.\n"
           "Lat Line: 1500 ft.\n"
           "Toy Hose: 750 ft.\n\n"
@@ -118,7 +121,11 @@ void main() {
           "Toy Wye: 15\n\n"
           "Folda-tank: 2\n"
           "Mark 3 + Kits: 1\n"
-          "Pump Mix (Gallons): 6\n\n";
+          "Pump Mix (Cans): 6\n\n"
+          "Water (Pallets): 0\n"
+          "Gatorade (Pallets): 0\n"
+          "MRE (Pallets): 0\n"
+          "Port-a-Potties: 0\n\n";
 
       //Act
       var actual = estimate.flatFireOrderText();
@@ -129,9 +136,7 @@ void main() {
 
     test('toCopyString that is 20+ acres', () {
       //Arrange
-      Estimate estimate = new Estimate();
-      int acres = 20;
-      estimate.acres = acres;
+      Estimate estimate = new Estimate(acres: 20);
       var expected = "Trunk Line: 5000 ft.\n"
           "Lat Line: 2500 ft.\n"
           "Toy Hose: 1250 ft.\n\n"
@@ -141,13 +146,13 @@ void main() {
           "Forester Nozzles: 25\n"
           "Toy Nozzles: 25\n"
           "Toy Wye: 25\n\n"
-          "Folda-tank: ${acres ~/ 5}\n"
-          "Mark 3 + Kits: ${acres ~/ 10 * 6}\n"
-          "Pump Mix (Gallons): ${((acres ~/ 5) ~/ 2) * 3}\n\n"
-          "Water (Pallets): ${acres ~/ 20}\n"
-          "Gatorade (Pallets): ${acres ~/ 20}\n"
-          "MRE (Pallets): ${acres ~/ 20}\n"
-          "Port-a-Potties: ${acres ~/ 2}\n\n";
+          "Folda-tank: 4\n"
+          "Mark 3 + Kits: 2\n"
+          "Pump Mix (Cans): 12\n\n"
+          "Water (Pallets): 1\n"
+          "Gatorade (Pallets): 1\n"
+          "MRE (Pallets): 1\n"
+          "Port-a-Potties: 2\n\n";
 
       //Act
       var actual = estimate.flatFireOrderText();
