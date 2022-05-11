8c490939e390af470a2ebd450fcb963dbda5f7cf
OconnellErin <oconerin@oregonstate.edu>
2022-05-11 09:49:53 -0700

    App.dart: Remove main screen class from file, add main_screen.dart import

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'route_map.dart';
-import 'screens/active_engagement_list_screen.dart';
-import 'screens/inactive_engagement_list_screen.dart';
-import 'event_handlers/floating_action_button_handler.dart';
-import 'event_handlers/popup_menu_button_handler.dart';
-import 'widgets/hidable_floating_action_button.dart';
-import 'widgets/side_drawer.dart';
-import 'widgets/sort_popup_menu_button.dart';
+import '/screens/main_screen.dart';
 
 class App extends StatelessWidget {
   static const APP_TITLE = 'Ops Normal';
@@ -22,46 +16,5 @@ class App extends StatelessWidget {
                 const AppBarTheme(color: const Color.fromRGBO(0, 80, 47, 1)),
             visualDensity: VisualDensity.adaptivePlatformDensity),
         home: MainScreen());
-  }
-}
-
-class MainScreen extends StatelessWidget {
-  static const tabs = [
-    Tab(text: 'Home', icon: Icon(Icons.home)),
-    Tab(text: 'Archive', icon: Icon(Icons.archive))
-  ];
-
-  final popupMenuButtonHandler = PopupMenuButtonHandler();
-  final floatingActionButtonHandler = FloatingActionButtonHandler();
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: tabs.length,
-        child: Scaffold(
-            floatingActionButton: HidableFloatingActionButton(
-                visible: true,
-                onPressed: () => floatingActionButtonHandler.onPressed(),
-                tooltip: 'New engagement',
-                child: const Icon(Icons.add)),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerDocked,
-            drawer: SideDrawer(),
-            appBar: AppBar(title: const Text('Ops Normal'), actions: [
-              SortPopupMenuButton(
-                  popupMenuButtonHandler: popupMenuButtonHandler)
-            ]),
-            body: SafeArea(
-                child: TabBarView(children: [
-              ActiveEngagementListScreen(
-                  popupMenuButtonHandler: popupMenuButtonHandler,
-                  floatingActionButtonHandler: floatingActionButtonHandler),
-              InactiveEngagementListScreen(
-                popupMenuButtonHandler: popupMenuButtonHandler,
-              ),
-            ])),
-            bottomNavigationBar: Container(
-                color: Colors.blueGrey[900],
-                child: TabBar(tabs: tabs, unselectedLabelColor: Colors.grey))));
   }
 }
