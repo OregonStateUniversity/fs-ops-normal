0c5e1ca212afbf17dde9a4f8de5eb34824ae06df
OconnellErin <oconerin@oregonstate.edu>
2022-05-11 09:49:15 -0700

    Main_Screen.dart: Add main_screen.dart file

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,49 @@
+import 'active_engagement_list_screen.dart';
+import 'inactive_engagement_list_screen.dart';
+import '/event_handlers/floating_action_button_handler.dart';
+import '/event_handlers/popup_menu_button_handler.dart';
+import '/widgets/hidable_floating_action_button.dart';
+import '/widgets/side_drawer.dart';
+import '/widgets/sort_popup_menu_button.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  static const tabs = [
+    Tab(text: 'Home', icon: Icon(Icons.home)),
+    Tab(text: 'Archive', icon: Icon(Icons.archive))
+  ];
+
+  final popupMenuButtonHandler = PopupMenuButtonHandler();
+  final floatingActionButtonHandler = FloatingActionButtonHandler();
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(
+            floatingActionButton: HidableFloatingActionButton(
+                visible: true,
+                onPressed: () => floatingActionButtonHandler.onPressed(),
+                tooltip: 'New engagement',
+                child: const Icon(Icons.add)),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            drawer: SideDrawer(),
+            appBar: AppBar(title: const Text('Ops Normal'), actions: [
+              SortPopupMenuButton(
+                  popupMenuButtonHandler: popupMenuButtonHandler)
+            ]),
+            body: SafeArea(
+                child: TabBarView(children: [
+              ActiveEngagementListScreen(
+                  popupMenuButtonHandler: popupMenuButtonHandler,
+                  floatingActionButtonHandler: floatingActionButtonHandler),
+              InactiveEngagementListScreen(
+                popupMenuButtonHandler: popupMenuButtonHandler,
+              ),
+            ])),
+            bottomNavigationBar: Container(
+                color: Colors.blueGrey[900],
+                child: TabBar(tabs: tabs, unselectedLabelColor: Colors.grey))));
+  }
+}
