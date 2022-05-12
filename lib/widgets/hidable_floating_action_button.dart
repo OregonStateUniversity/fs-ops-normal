98409d14325ed5876b1896807de4a59825f417ca
Yong Bakos <ybakos@humanoriented.com>
2022-05-12 00:20:09 -0700

    HidableFloatingActionButton: Require params and remove nullable types.
    References #150.

--- lib/widgets/hidable_floating_action_button.dart
+++ lib/widgets/hidable_floating_action_button.dart
@@ -3,22 +3,23 @@ import 'package:flutter/material.dart';
 class HidableFloatingActionButton extends StatelessWidget {
 
   final bool visible;
-  final String? tooltip;
-  final Widget? child;
-  final VoidCallback? onPressed;
+  final String tooltip;
+  final Widget child;
+  final VoidCallback onPressed;
 
   const HidableFloatingActionButton({Key? key, required this.visible,
-    this.tooltip, this.child, this.onPressed }) : super(key: key);
+    required this.tooltip, required this.child, required this.onPressed }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Visibility(
       visible: this.visible,
       child: FloatingActionButton(
-        onPressed: this.onPressed,
         tooltip: this.tooltip,
         child: this.child,
-      ),
+        onPressed: this.onPressed
+      )
     );
   }
+
 }
