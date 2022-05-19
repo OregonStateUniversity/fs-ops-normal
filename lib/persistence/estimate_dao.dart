5ea1a087df270dc34a5d704698fe8aff69d3909e
Yong Bakos <ybakos@humanoriented.com>
2022-05-19 13:16:15 -0700

    EstimateDAO: Add delete method.

--- lib/persistence/estimate_dao.dart
+++ lib/persistence/estimate_dao.dart
@@ -5,10 +5,16 @@ import '../models/estimate.dart';
 class EstimateDAO {
 
   static const SQL_SELECT = "SELECT * FROM estimates WHERE engagementId = ? ORDER BY createdAt DESC;";
+  static const SQL_DELETE = "DELETE FROM estimates WHERE estimateId = ?;";
 
   static Future<List<Estimate>> estimates({required DatabaseManager databaseManager, required Engagement engagement}) async {
     final estimateRecords = await databaseManager.selectWhere(sql: SQL_SELECT, values: [engagement.id]);
     return estimateRecords.map( (row) => Estimate.fromMap(map: row) ).toList();
+  }
+
+  static Future<void> delete({required DatabaseManager databaseManager, required Estimate estimate}) async {
+    final id = estimate.id;
+    if (id != null) databaseManager.delete(sql: SQL_DELETE, id: id);
   }
 
 }
