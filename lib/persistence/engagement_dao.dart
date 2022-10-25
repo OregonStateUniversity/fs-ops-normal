8ccb5f0cb700a17bd55807c76274894a3cd36fbf
Ben Shiffman <notbenshiffman@gmail.com>
2022-10-25 20:56:45 +0200

    Squelched linter warnings References #188

--- lib/persistence/engagement_dao.dart
+++ lib/persistence/engagement_dao.dart
@@ -3,20 +3,25 @@ import 'engagement_dto.dart';
 import '../models/engagement.dart';
 
 class EngagementDAO {
-  
-  static const SQL_SELECT_ALL = "SELECT * FROM engagements ORDER BY createdAt DESC;";
-  static const SQL_SELECT_ACTIVE = "SELECT * FROM engagements WHERE active = 1 ORDER BY createdAt DESC;";
-  static const SQL_SELECT_INACTIVE = "SELECT * FROM engagements WHERE active = 0 ORDER BY createdAt DESC;";
-  static const SQL_SELECT_ONE = "SELECT * FROM engagements WHERE id = ?;";
-  static const SQL_INSERT = "INSERT INTO engagements(name, createdAt, active) VALUES (?, ?, ?);";
-  static const SQL_DEACTIVATE = "UPDATE engagements SET active = 0 WHERE id = ?;";
-  static const SQL_REACTIVATE = "UPDATE engagements SET active = 1 WHERE id = ?;";
-  static const SQL_DELETE = "DELETE FROM engagements WHERE id = ?;";
+  static const sqlSelectAll =
+      "SELECT * FROM engagements ORDER BY createdAt DESC;";
+  static const sqlSelectActive =
+      "SELECT * FROM engagements WHERE active = 1 ORDER BY createdAt DESC;";
+  static const sqlSelectInactive =
+      "SELECT * FROM engagements WHERE active = 0 ORDER BY createdAt DESC;";
+  static const sqlSelectOne = "SELECT * FROM engagements WHERE id = ?;";
+  static const sqlInsert =
+      "INSERT INTO engagements(name, createdAt, active) VALUES (?, ?, ?);";
+  static const sqlDeactivate =
+      "UPDATE engagements SET active = 0 WHERE id = ?;";
+  static const sqlReactivate =
+      "UPDATE engagements SET active = 1 WHERE id = ?;";
+  static const sqlDelete = "DELETE FROM engagements WHERE id = ?;";
 
   static Future<List<Engagement>> activeEngagements(
       {required DatabaseManager databaseManager}) async {
     final engagementRecords =
-        await databaseManager.select(sql: SQL_SELECT_ACTIVE);
+        await databaseManager.select(sql: sqlSelectActive);
     return engagementRecords
         .map((row) => Engagement.fromMap(map: row))
         .toList();
@@ -25,7 +30,7 @@ class EngagementDAO {
   static Future<List<Engagement>> inactiveEngagements(
       {required DatabaseManager databaseManager}) async {
     final engagementRecords =
-        await databaseManager.select(sql: SQL_SELECT_INACTIVE);
+        await databaseManager.select(sql: sqlSelectInactive);
     return engagementRecords
         .map((row) => Engagement.fromMap(map: row))
         .toList();
@@ -35,32 +40,25 @@ class EngagementDAO {
       {required DatabaseManager databaseManager,
       required EngagementDTO dto}) async {
     databaseManager.insert(
-        sql: SQL_INSERT,
+        sql: sqlInsert,
         values: [dto.name, dto.createdAt.toString(), dto.active]);
   }
 
   static Future delete(
       {required DatabaseManager databaseManager,
       required Engagement engagement}) async {
-    databaseManager.delete(
-        sql: SQL_DELETE,
-        id: engagement.id!);
+    databaseManager.delete(sql: sqlDelete, id: engagement.id!);
   }
 
   static Future reactivate(
       {required DatabaseManager databaseManager,
       required Engagement engagement}) async {
-    databaseManager.update(
-        sql: SQL_REACTIVATE,
-        values: [engagement.id!]);
+    databaseManager.update(sql: sqlReactivate, values: [engagement.id!]);
   }
 
   static Future deactivate(
       {required DatabaseManager databaseManager,
       required Engagement engagement}) async {
-    databaseManager.update(
-      sql: SQL_DEACTIVATE,
-      values: [engagement.id!]);
+    databaseManager.update(sql: sqlDeactivate, values: [engagement.id!]);
   }
-
 }
