4683f8e12f4ca6633926f4eb112ff4b1626f0a3e
Ben Shiffman <notbenshiffman@gmail.com>
2022-10-25 21:02:22 +0200

    Squelched linter warnings References #192

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,50 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class AboutScreen extends StatelessWidget {
   static const routeName = 'about';
   static const title = "About";
 
   static const TextAlign align = TextAlign.center;
-  static const SOURCE_CODE_URL =
-      "https://github.com/osu-cascades/fs-hose-jockey";
+  static const sourceCodeURL = "https://github.com/osu-cascades/fs-hose-jockey";
+
+  const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: const Text(title),
       ),
       body: Column(
         children: [
           Align(
             alignment: FractionalOffset.center,
             child: Container(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                  Text(
+                  const Text(
                     "Created By Ben Shiffman,\nIan Snyder, Erin O'Connell, \n Josh C., Kyle H. for",
                     textAlign: align,
                   ),
-                  Text(
+                  const Text(
                     "U.S. Forest Service",
                     textAlign: align,
                   ),
-                  Divider(),
-                  Text(
+                  const Divider(),
+                  const Text(
                     "Ops Normal is an application designed for wildland firefighters to "
                     "quickly estimate the amount of equipment that they will need to fight a fire.",
                     textAlign: align,
                   ),
-                  Divider(),
-                  Text(
+                  const Divider(),
+                  const Text(
                     "\nThe source code for this project can be found at: ",
                     textAlign: align,
                   ),
                   TextButton(
-                      onPressed: () => launch(SOURCE_CODE_URL),
-                      child: Text(SOURCE_CODE_URL))
+                      onPressed: () => launchUrlString(sourceCodeURL),
+                      child: const Text(sourceCodeURL))
                 ],
               ),
             ),
