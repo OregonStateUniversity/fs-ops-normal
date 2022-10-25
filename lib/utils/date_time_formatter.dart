bad8e3fc90438248783a772744b734e7ccc611fb
Kelson Denton <denton.kelson@gmail.com>
2022-10-24 20:51:58 -0700

    Squelched linter warnings. References #205

--- lib/utils/date_time_formatter.dart
+++ lib/utils/date_time_formatter.dart
@@ -1,9 +1,9 @@
 import 'package:intl/intl.dart';
 
-const FULL_DATE_AND_TIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
+const fullDateAndTimeFormat = "dd/MM/yyyy HH:mm:ss";
 
 class DateTimeFormatter {
-  static final formatter = DateFormat(FULL_DATE_AND_TIME_FORMAT);
+  static final formatter = DateFormat(fullDateAndTimeFormat);
 
   static String format(DateTime dateTime) {
     return formatter.format(dateTime);
