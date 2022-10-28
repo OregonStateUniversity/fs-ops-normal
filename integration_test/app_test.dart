dfb6200942c23bc1f59af7a498b44491b40033f4
aimeekb <boglea@oregonstate.edu>
2022-10-28 10:07:28 -0700

    Squelch linter warning in app_test.dart, Issue #181. Transformed constructor to named const, per Dart best practice.

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -4,7 +4,7 @@ import 'package:ops_normal/app.dart';
 void main() {
   group('App', () {
     testWidgets('Starts empty', (tester) async {
-      await tester.pumpWidget(App());
+      await tester.pumpWidget(const App());
       await tester.pumpAndSettle();
       var finder = find.text('Ops Normal');
       expect(finder, findsOneWidget);
