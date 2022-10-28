87e4c288899d9bbd7e7f3f95f2722d6a4ea84b60
aimeekb <boglea@oregonstate.edu>
2022-10-27 18:08:11 -0700

    Squelch linter warnings in Engagement.dart file, issue # 185. Corrected issue with unnecessary use of 'this' to access a class member, and removed braces from string interpolation statements where not needed.

--- lib/models/engagement.dart
+++ lib/models/engagement.dart
@@ -1,27 +1,30 @@
 import 'estimate.dart';
 
 class Engagement {
-  
   final int? id;
   final String name;
   final DateTime createdAt;
   final bool active;
   final List<Estimate> estimates;
 
-  Engagement({this.id, required this.name, required this.createdAt, required this.estimates, required this.active});
+  Engagement(
+      {this.id,
+      required this.name,
+      required this.createdAt,
+      required this.estimates,
+      required this.active});
 
   @override
   String toString() {
-    return 'Engagement ${this.id}: ${this.name}, ${this.createdAt}';
+    return 'Engagement $id: $name, $createdAt';
   }
 
   get isSaved => id != null;
 
-  Engagement.fromMap({required Map map}) :
-    id = map['id'],
-    name = map['name'],
-    createdAt = DateTime.parse(map['createdAt']),
-    active = map['active'] == 1,
-    estimates = [];
-
+  Engagement.fromMap({required Map map})
+      : id = map['id'],
+        name = map['name'],
+        createdAt = DateTime.parse(map['createdAt']),
+        active = map['active'] == 1,
+        estimates = [];
 }
