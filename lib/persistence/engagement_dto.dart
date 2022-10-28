4ff942c63a177c944324aa627cf97d23fbcb8d1f
Azzam <azzamalimc2@gmail.com>
2022-10-28 10:58:45 -0700

    linter warnings squelched

--- lib/persistence/engagement_dto.dart
+++ lib/persistence/engagement_dto.dart
@@ -1,12 +1,10 @@
 class EngagementDTO {
-
   final String name;
   final bool active;
   final DateTime createdAt;
-  
-  EngagementDTO({required this.name, required this.active, required this.createdAt});
 
-  String toString() =>
-    'Name: $name, Active: $active, Created At: $createdAt';
-
+  EngagementDTO(
+      {required this.name, required this.active, required this.createdAt});
+  @override
+  String toString() => 'Name: $name, Active: $active, Created At: $createdAt';
 }
