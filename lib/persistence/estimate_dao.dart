7256fcf8304db8dfe5b41722521b75d91d79dbd2
Kelson Denton <denton.kelson@gmail.com>
2022-11-01 18:10:30 -0700

    Squelched linter warnings. References #190.

--- lib/persistence/estimate_dao.dart
+++ lib/persistence/estimate_dao.dart
@@ -5,22 +5,22 @@ import '../models/estimate.dart';
 
 class EstimateDAO {
 
-  static const SQL_SELECT = "SELECT * FROM estimates WHERE engagementId = ? ORDER BY createdAt DESC;";
-  static const SQL_DELETE = "DELETE FROM estimates WHERE estimateId = ?;";
-  static const SQL_INSERT = "INSERT INTO estimates(engagementId, name, createdAt, acres, structures) VALUES (?, ?, ?, ?, ?);";
+  static const sqlSelect = "SELECT * FROM estimates WHERE engagementId = ? ORDER BY createdAt DESC;";
+  static const sqlDelete = "DELETE FROM estimates WHERE estimateId = ?;";
+  static const sqlInsert = "INSERT INTO estimates(engagementId, name, createdAt, acres, structures) VALUES (?, ?, ?, ?, ?);";
 
   static Future<List<Estimate>> estimates({required DatabaseManager databaseManager, required Engagement engagement}) async {
-    final estimateRecords = await databaseManager.selectWhere(sql: SQL_SELECT, values: [engagement.id]);
+    final estimateRecords = await databaseManager.selectWhere(sql: sqlSelect, values: [engagement.id]);
     return estimateRecords.map( (row) => Estimate.fromMap(map: row) ).toList();
   }
 
   static Future<void> delete({required DatabaseManager databaseManager, required Estimate estimate}) async {
     final id = estimate.id;
-    if (id != null) databaseManager.delete(sql: SQL_DELETE, id: id);
+    if (id != null) databaseManager.delete(sql: sqlDelete, id: id);
   }
 
   static Future<void> save({required DatabaseManager databaseManager, required EstimateDTO dto}) async {
-    databaseManager.insert(sql: SQL_INSERT, values: [dto.engagementId, dto.name, dto.createdAt.toString(), dto.acres, dto.structures]);
+    databaseManager.insert(sql: sqlInsert, values: [dto.engagementId, dto.name, dto.createdAt.toString(), dto.acres, dto.structures]);
   }
 
 }
