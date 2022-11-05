a5f8b267982a8c9789f4db33471c9d78c57577c5
Azzam <azzamalimc2@gmail.com>
2022-11-05 11:12:21 -0700

    use const constructor

--- lib/widgets/sort_popup_menu_button.dart
+++ lib/widgets/sort_popup_menu_button.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import '../event_handlers/popup_menu_button_handler.dart';
 
 class SortPopupMenuButton extends StatelessWidget {
-
   final PopupMenuButtonHandler popupMenuButtonHandler;
 
-  const SortPopupMenuButton({Key? key, required this.popupMenuButtonHandler}) : super(key: key);
+  const SortPopupMenuButton({Key? key, required this.popupMenuButtonHandler})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return PopupMenuButton(
-      icon: Icon(Icons.sort),
-      itemBuilder: (context) => [
-        PopupMenuItem(value: 'newest', child: const Text('Newest')),
-        PopupMenuItem(value: 'oldest', child: const Text('Oldest')),
-      ],
-      onSelected: (String value) => popupMenuButtonHandler.onSelected(value)
-    );
+        icon: const Icon(Icons.sort),
+        itemBuilder: (context) => [
+              const PopupMenuItem(value: 'newest', child: Text('Newest')),
+              const PopupMenuItem(value: 'oldest', child: Text('Oldest')),
+            ],
+        onSelected: (String value) => popupMenuButtonHandler.onSelected(value));
   }
 }
