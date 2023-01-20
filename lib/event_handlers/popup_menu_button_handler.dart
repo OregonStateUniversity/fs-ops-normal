f68f2f6099fda8fbbc212ded6b1f831785cfff5a
Yong Bakos <ybakos@humanoriented.com>
2023-01-20 09:50:52 -0800

    PopupMenuButtonHandler: Remove comment.

--- lib/event_handlers/popup_menu_button_handler.dart
+++ lib/event_handlers/popup_menu_button_handler.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 class PopupMenuButtonHandler {
   void Function(String value) onSelected = (value) {
-    //log: only print the debug mode. won't print the release mode
     log("Warning: This event handler should be assigned a new function.");
   };
 }
