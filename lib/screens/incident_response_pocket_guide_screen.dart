ceae88178e4b4cd97b5ed9c52efa8d61e55eed58
Kelson Denton <denton.kelson@gmail.com>
2023-02-07 22:40:36 -0800

    Extracted widgets and refactored. References #245.

--- lib/screens/incident_response_pocket_guide_screen.dart
+++ lib/screens/incident_response_pocket_guide_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ops_normal/screens/compass_screen.dart';
 import 'package:pdfx/pdfx.dart';
-
 import '../widgets/bottom_nav_bar.dart';
 
 class IncidentResponsePocketGuideScreen extends StatelessWidget {
@@ -22,45 +21,9 @@ class IncidentResponsePocketGuideScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          SizedBox(
-            height: 150,
-            child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const CompassScreen()));
-                },
-                child: Image.asset('assets/images/compass_image.png')),
-          ),
+          const PdfViewer(),
           Expanded(child: pdfView()),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12),
-                child: ElevatedButton(
-                    child: const Text("Go To"),
-                    onPressed: () {
-                      pageController.text == "" ? pageController.clear():
-                        pdfController
-                            .jumpToPage(int.parse(pageController.text) + 16);
-                        FocusScope.of(context).unfocus();
-                        pageController.clear();
-                    }),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: TextField(
-                    controller: pageController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: "Page Number"),
-                  ),
-                ),
-              )
-            ],
-          )
+          PageSearchBar(pageController: pageController, pdfController: pdfController)
         ],
       ),
       bottomNavigationBar: const BottomNavBar(),
@@ -70,4 +33,104 @@ class IncidentResponsePocketGuideScreen extends StatelessWidget {
   Widget pdfView() => PdfView(
         controller: pdfController,
       );
+}
+
+// widget that displays pdf that is swipeable
+class PdfViewer extends StatelessWidget {
+  const PdfViewer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const CompassScreen()));
+          },
+          child: Image.asset('assets/images/compass_image.png')
+        ),
+    );
+  }
+}
+
+// widget containing search bar and button to go to required page
+class PageSearchBar extends StatelessWidget {
+  const PageSearchBar({
+    super.key,
+    required this.pageController,
+    required this.pdfController,
+  });
+
+  final TextEditingController pageController;
+  final PdfController pdfController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: GoToButton(pageController: pageController, pdfController: pdfController),
+        ),
+        SearchEntry(pageController: pageController)
+      ],
+    );
+  }
+}
+
+// widget to enter desired page number to be searched
+class SearchEntry extends StatelessWidget {
+  const SearchEntry({
+    super.key,
+    required this.pageController,
+  });
+
+  final TextEditingController pageController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: TextField(
+          controller: pageController,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: "Page Number"),
+        ),
+      ),
+    );
+  }
+}
+
+// button clicked to go to desired page
+class GoToButton extends StatelessWidget {
+  const GoToButton({
+    super.key,
+    required this.pageController,
+    required this.pdfController,
+  });
+
+  final TextEditingController pageController;
+  final PdfController pdfController;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: const Text("Go To"),
+        onPressed: () {
+          pageController.text == "" ? pageController.clear():
+            pdfController
+                .jumpToPage(int.parse(pageController.text) + 16);
+            FocusScope.of(context).unfocus();
+            pageController.clear();
+        }
+      );
+  }
+
 }
