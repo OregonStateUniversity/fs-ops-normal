416cbd5df4a0c1a8eccc5f09f4e3f03b1741a487
aimeekb <boglea@oregonstate.edu>
2023-02-11 16:33:19 -0800

    Ref. #253 Modified the estimate_screen.dart to allow the DataTables to expand to match the screen size, in order to fix the RenderFlex error. Wrapped the DataTables in a SizedBox that would allow the width to expand, and I modified the horizontal margins in the DataTable parameters to make layout neater.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import location
 import path_provider_foundation
 import pdfx
 import share_plus
-import shared_preferences_macos
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
