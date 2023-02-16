8f6302c0691f2f484080a0288a12c6d9709343b7
Kelson Denton <denton.kelson@gmail.com>
2023-02-15 23:21:38 -0800

    Resolving conflicts

--- lib/screens/new_estimate_screen.dart
+++ lib/screens/new_estimate_screen.dart
@@ -5,14 +5,21 @@ import '../models/estimate.dart';
 import '../models/engagement.dart';
 import '../utils/date_time_formatter.dart';
 
-//const List<String> fireShape = <String>['Shape 1', 'Shape 2', 'Shape 3'];
 const List<String> fireType = <String>['Grass', 'Timber'];
-const List<String> fireShape = <String>['assets/images/fireShape1.png',
-'assets/images/fireShape2.png',
-'assets/images/fireShape3.png',
-'assets/images/fireShape4.png',
-'assets/images/fireShape5.png',
-'assets/images/fireShape6.png'];
+const List<String> fireShape = [
+  'assets/images/fireShape1.png',
+  'assets/images/fireShape2.png',
+  'assets/images/fireShape3.png',
+  'assets/images/fireShape4.png',
+  'assets/images/fireShape5.png',
+  'assets/images/fireShape6.png'];
+const Map<String, int> fireShapeMap = {
+  'assets/images/fireShape1.png': 1,
+  'assets/images/fireShape2.png': 2,
+  'assets/images/fireShape3.png': 3,
+  'assets/images/fireShape4.png': 4,
+  'assets/images/fireShape5.png': 5,
+  'assets/images/fireShape6.png': 6};
 
 class NewEstimateScreen extends StatefulWidget {
   static const routeName = 'newEstimateScreen';
@@ -25,17 +32,27 @@ class NewEstimateScreen extends StatefulWidget {
 }
 
 class _NewEstimateScreenState extends State<NewEstimateScreen> {
-  var formKey = GlobalKey<FormState>();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   var fireShapeVal = null;
   var fireTypeVal = null;
   var firePerimeter = null;
 
+<<<<<<< HEAD
   var myControllerAcreage = TextEditingController(text: "");
   var myControllerStructure = TextEditingController(text: "");
+=======
+  var imgs = null;
+
+  var myControllerAcreage = TextEditingController(text: " ");
+  var myControllerStructure = TextEditingController(text: " ");
+  var myControllerPerimeter = TextEditingController(text: " ");
+>>>>>>> 847fd18 (Resolving conflicts)
 
   static const bool _acreageInputIsValid = true;
   static const bool _structureInputIsValid = true;
+  static const bool _perimeterInputIsValid = true;
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +73,7 @@ class _NewEstimateScreenState extends State<NewEstimateScreen> {
           const Text('--OR--'),
           OutlinedButton(
               onPressed: () {
-                _dialogBuilder(context);
+                showInformationDialog(context);
               },
               child: const Text("Calculate Acerage")),
           const Padding(padding: EdgeInsets.all(10)),
@@ -107,7 +124,7 @@ class _NewEstimateScreenState extends State<NewEstimateScreen> {
                 var acreseDouble = double.parse(myControllerAcreage.text);
                 var estimate = Estimate(
                     acres: acreseDouble.toInt(),
-                    structures: int.parse(myControllerStructure.text),
+                    structures: int.parse(myControllerStructure.text),         
                     timeStamp: DateTimeFormatter.format(DateTime.now()));
                 myControllerAcreage.text.isNotEmpty
                     ? Navigator.push(
@@ -126,60 +143,97 @@ class _NewEstimateScreenState extends State<NewEstimateScreen> {
     );
   }
 
-  Future<void> _dialogBuilder(BuildContext context) {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Fire Parameters'),
-          content: const Text('Enter the perimeter in chains (1 chain = 66ft) and select the rough shape to caclulate the acreage.'),
-          actions: <Widget>[
 
-            // text box to enter fire perimeter in chains
-            TextField(
-              controller: myControllerAcreage,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                  labelText: 'Enter Perimeter (in chains)',
-                  errorText: _acreageInputIsValid ? null : 'error',
-                  border: OutlineInputBorder())),
-            const Padding(padding: EdgeInsets.all(10)),
+  Future<void> showInformationDialog(BuildContext context) async {
+    return await showDialog(
+        context: context,
+        builder: (context) {
+          bool isChecked = false;
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              content: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // enter perimeter of fire
+                      TextFormField(
+                        controller: myControllerPerimeter,
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          return value != "" ? null : "Enter Perimeter in Chains";
+                        },
+                        decoration:
+                            const InputDecoration(hintText: "Please Enter Perimeter"),
+                      ),
+                      // select fire shape
+                      DropdownButton<String>(
+                        hint: const Text('Select Fire Shape'),
+                        value: fireShapeVal,
+                        icon: const Icon(Icons.arrow_drop_down),
+                        isExpanded: true,
+                        onChanged: (String? value) {
+                          // This is called when the user selects an item.
+                          setState(() {
+                            fireShapeVal = value!;
+                            });
+                        },
+                        // entries in the dropdown
+                        items: fireShape.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Image.asset(value)
+                          );
+                        }).toList(),),
+                    ],
+                  )),
+              title: Text('Enter perimeter in chains and select rough shape.'),
+              actions: <Widget>[
+                InkWell(
+                  child: Text('OK   '),
+                  onTap: () {
+                    if (true) {
+                      myControllerAcreage.text = calculateAcreage();
+                      Navigator.of(context).pop();
+                    }
+                  },
+                ),
+              ],
+            );
+          });
+        });
 
-            // dropdown to select fire shape
-            DropdownButton<String>(
-            hint: const Text('Select Fire Shape'),
-            value: fireShapeVal,
-            icon: const Icon(Icons.arrow_drop_down),
-            isExpanded: true,
-            onChanged: (String? value) {
-              // This is called when the user selects an item.
-              setState(() {
-                fireShapeVal = value;
-              });
-            },
-            // entries in the dropdown
-            items: fireShape.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Image.asset(value)
-              );
-            }).toList(),),
-
-            // button to generate acreage
-            TextButton(
-              style: TextButton.styleFrom(
-                textStyle: Theme.of(context).textTheme.labelLarge,
-              ),
-              child: const Text('Calculate Acreage'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
+        
   }
+
+  String calculateAcreage() {
+    int? fireShapeNum = fireShapeMap[fireShapeVal];
+    int? perimeter = int.parse(myControllerPerimeter.text);
+    double calcAcres = 0;
+    switch(fireShapeNum) {
+      /// CALCULATIONS INCORRECT PLACEHOLDERS, NEED TO UPDATE
+      case 1:  // fire is a circle
+        calcAcres = perimeter * 0.7;
+        break;
+      case 2:  // fire is tall rectangle
+        calcAcres = perimeter * 0.6;
+        break;
+      case 3:  // shape is triangle or sideways rectangle
+        calcAcres = perimeter * 0.5;
+        break;
+      case 4:  // fire is close to square
+        calcAcres = perimeter * 0.4;
+        break;
+      case 5:  // amoeba shape 1
+        calcAcres = perimeter * 0.3;
+        break;
+      case 6:  // amoeba shape 2
+        calcAcres = perimeter * 0.2;
+        break;
+    }
+    return calcAcres.toString();
+  }
+
 
   
 
