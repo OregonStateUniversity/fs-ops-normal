5563fb3d0b203dc3ed57396057fb95ec00707130
Azzam <azzamalimc2@gmail.com>
2023-02-17 03:08:27 -0800

    remove classes and use a list to store instructions

--- lib/utils/how_to_instructions_strings.dart
+++ lib/utils/how_to_instructions_strings.dart
@@ -1,48 +1,106 @@
-class HowToCreateEngagementAndOrder {
-  static const title = "Creating Your First Engagement and Order";
-  static const description =
-      "1. On the home page click the bottom plus(+) button to create a new engagement. And engagement is a fire that you are currently working.\n"
-      "\t\t\ta. Give the engagement a name, something to describe the fire like location\n"
-      "\t\t\tb. Click the 'Create' button\n"
-      "\t\t\tc. You should see a new engagement show up on the screen with the name you've just entered."
-      "\n2. Tap on the newly created engagement.\n"
-      "\t\t\ta. Click on the bottom plus(+) button to create a new order.\n"
-      "\t\t\tb. Inside the popup input the acreage of the fire.\n"
-      "\t\t\tc. Click on the 'Calculate Estimate' button.\n"
-      "\t\t\td. On this page you are now able to edit the amount of equipment that you want. Once your numbers are correct, click the 'Save' button\n"
-      "\t\t\te. From here you will be able to see and copy the order that you've just created. To copy click the 'Tap Here To Copy' button.\n"
-      "\n3. Now that the order is done and copied to your clipboard you now send it to the individual who processes the delivery\n"
-      "\t\t\ta. Once you are done and want to create another order, tap the 'Back' button and it will take you back to the list of orders with your newly created order there as well\n"
-      "\t\t\tb. To go back and create another engagement tap the 'Home' button.";
-}
+// class HowToCreateEngagementAndOrder {
+//   static const title = "Creating Your First Engagement and Order";
+//   static const description =
+//       "1. On the home page click the bottom plus(+) button to create a new engagement. And engagement is a fire that you are currently working.\n"
+//       "\t\t\ta. Give the engagement a name, something to describe the fire like location\n"
+//       "\t\t\tb. Click the 'Create' button\n"
+//       "\t\t\tc. You should see a new engagement show up on the screen with the name you've just entered."
+//       "\n2. Tap on the newly created engagement.\n"
+//       "\t\t\ta. Click on the bottom plus(+) button to create a new order.\n"
+//       "\t\t\tb. Inside the popup input the acreage of the fire.\n"
+//       "\t\t\tc. Click on the 'Calculate Estimate' button.\n"
+//       "\t\t\td. On this page you are now able to edit the amount of equipment that you want. Once your numbers are correct, click the 'Save' button\n"
+//       "\t\t\te. From here you will be able to see and copy the order that you've just created. To copy click the 'Tap Here To Copy' button.\n"
+//       "\n3. Now that the order is done and copied to your clipboard you now send it to the individual who processes the delivery\n"
+//       "\t\t\ta. Once you are done and want to create another order, tap the 'Back' button and it will take you back to the list of orders with your newly created order there as well\n"
+//       "\t\t\tb. To go back and create another engagement tap the 'Home' button.";
+// }
 
-class HowToDeleteEngagementOrOrder {
-  static const title = "How to Delete an Engagement or Order";
-  static const description =
-      "1. Find the engagement or order that you want to delete.\n"
-      "\t\t\ta. Tap and hold the left side of the screen on the engagement or order and slide it to the right.\n"
-      "\t\t\tb. A popup will ask you to confirm the deletion. Tap on the 'Delete' button. Warning: This cannot be undone.";
-}
+// class HowToDeleteEngagementOrOrder {
+//   static const title = "How to Delete an Engagement or Order";
+//   static const description =
+//       "1. Find the engagement or order that you want to delete.\n"
+//       "\t\t\ta. Tap and hold the left side of the screen on the engagement or order and slide it to the right.\n"
+//       "\t\t\tb. A popup will ask you to confirm the deletion. Tap on the 'Delete' button. Warning: This cannot be undone.";
+// }
 
-class HowToArchiveEngagement {
-  static const title = "How to Archive an Engagement";
-  static const description =
-      "1. On the main page find the engagement that you wish to archive.\n"
-      "\t\t\ta. Tap and hold the left side of the screen on the engagement and slide to the right.\n"
-      "\t\t\tb. A popup will ask you to confirm the archive. Tap on the 'Yes' button to confirm.\n";
-}
+// class HowToArchiveEngagement {
+//   static const title = "How to Archive an Engagement";
+//   static const description =
+//       "1. On the main page find the engagement that you wish to archive.\n"
+//       "\t\t\ta. Tap and hold the left side of the screen on the engagement and slide to the right.\n"
+//       "\t\t\tb. A popup will ask you to confirm the archive. Tap on the 'Yes' button to confirm.\n";
+// }
 
-class HowToUnarchiveEngagement {
-  static const title = "How to Unarchive an Engagement";
-  static const description =
-      "1. First you need to get to the archive page by tapping on the 'Archive' button in the bottom left corner.\n"
-      "\t\t\ta. Once you are on this page you now need to locate the engagement that you want to un archive.\n"
-      "\t\t\tb. Once located you need to tap and hold the left side of the screen on the engagement and slide it to the right.\n"
-      "\t\t\tc. A popup will ask you to confirm the unarchive. Tap on the 'Yes' button to confirm.\n";
-}
+// class HowToUnarchiveEngagement {
+//   static const title = "How to Unarchive an Engagement";
+//   static const description =
+//       "1. First you need to get to the archive page by tapping on the 'Archive' button in the bottom left corner.\n"
+//       "\t\t\ta. Once you are on this page you now need to locate the engagement that you want to un archive.\n"
+//       "\t\t\tb. Once located you need to tap and hold the left side of the screen on the engagement and slide it to the right.\n"
+//       "\t\t\tc. A popup will ask you to confirm the unarchive. Tap on the 'Yes' button to confirm.\n";
+// }
 
-class HowToChangeVisualSettings {
-  static const title = "How to Change Visual Settings";
-  static const description =
-      "To change the view of the application tap on the 'Visual Settings' button inside the left hand slide out drawer ";
-}
+// class HowToChangeVisualSettings {
+//   static const title = "How to Change Visual Settings";
+//   static const description =
+//       "To change the view of the application tap on the 'Visual Settings' button inside the left hand slide out drawer ";
+// }
+
+List instructionList = [
+  {
+    "title": "Creating Your First Engagement and Order",
+    "description": [
+      "On the home page click the bottom plus(+) button to create a new engagement. And engagement is a fire that you are currently working.",
+      [
+        "Give the engagement a name, something to describe the fire like location",
+        "Click the 'Create' button",
+        "You should see a new engagement show up on the screen with the name you've just entered.",
+      ],
+      "Tap on the newly created engagement.",
+      [
+        "Click on the bottom plus(+) button to create a new order.",
+        "Inside the popup input the acreage of the fire.",
+        "Click on the 'Calculate Estimate' button.",
+        "On this page you are now able to edit the amount of equipment that you want. Once your numbers are correct, click the 'Save' button",
+        "From here you will be able to see and copy the order that you've just created. To copy click the 'Tap Here To Copy' button.",
+      ],
+      "Now that the order is done and copied to your clipboard you now send it to the individual who processes the delivery",
+      [
+        "Once you are done and want to create another order, tap the 'Back' button and it will take you back to the list of orders with your newly created order there as well\n"
+            "To go back and create another engagement tap the 'Home' button."
+      ]
+    ]
+  },
+  {
+    "title": "How to Delete an Engagement or Order",
+    "description": [
+      "Find the engagement or order that you want to delete.",
+      "Tap and hold the left side of the screen on the engagement or order and slide it to the right.",
+      "A popup will ask you to confirm the deletion. Tap on the 'Delete' button. Warning: This cannot be undone."
+    ]
+  },
+  {
+    "title": "How to Archive an Engagement",
+    "description": [
+      "On the main page find the engagement that you wish to archive.",
+      "Tap and hold the left side of the screen on the engagement and slide to the right.",
+      "A popup will ask you to confirm the archive. Tap on the 'Yes' button to confirm.",
+    ]
+  },
+  {
+    "title": "How to Unarchive an Engagement",
+    "description": [
+      "First you need to get to the archive page by tapping on the 'Archive' button in the bottom left corner.",
+      "Once you are on this page you now need to locate the engagement that you want to un archive",
+      "Once located you need to tap and hold the left side of the screen on the engagement and slide it to the right.",
+      "A popup will ask you to confirm the unarchive. Tap on the 'Yes' button to confirm.",
+    ]
+  },
+  {
+    'title': "How to Change Visual Settings",
+    "description": [
+      "To change the view of the application tap on the 'Visual Settings' button inside the left hand slide out drawer "
+    ]
+  }
+];
