26c1b5df9ea8712cf26de617d81513158b82981b
Azzam <azzamalimc2@gmail.com>
2023-03-02 23:13:13 -0800

    create packet to store constant styles

---
+++ lib/utils/ops_normal_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+///********************* */
+///This file is created to store constant styles - fonts, colors ...etc
+///********************* */
+
+final TextStyle kLocationCardTitleStyle = TextStyle(
+    fontSize: 15, fontWeight: FontWeight.bold, color: Colors.green.shade800);
