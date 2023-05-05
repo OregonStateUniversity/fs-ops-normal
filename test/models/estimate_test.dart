d0f8e6b49b7f8ce5dd64313d24eaea2e6444bdb2
Kelson Denton <denton.kelson@gmail.com>
2023-05-04 20:42:05 -0700

    Revised tests for defaultOnePointFiveInchHose. Added test cases for when there are exactly 10 and exactly 40 structures. References #288.

--- test/models/estimate_test.dart
+++ test/models/estimate_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:test/test.dart';
 import 'package:ops_normal/models/estimate.dart';
 
@@ -519,10 +521,6 @@ void main() {
 
     // STRUCTURE TESTS BELOW
 
-    test('Default Sprinklers - Structures: 0', () {
-      //Arrange
-      Estimate estimate = Estimate(acres: 0, structures: 0);
-      var expected = 0;
     //Sprinklers
     group('Default Sprinklers', () {
       test('Structures = 0 -> Sprinkler = 0', () {
@@ -537,10 +535,6 @@ void main() {
         expect(actual, equals(expected));
       });
 
-    test('Default Sprinklers - Structures: 0 < x < 10 ', () {
-      //Arrange
-      Estimate estimate = Estimate(acres: 0, structures: 5);
-      var expected = 10;
       test(' Structures < 10 -> Sprinkler = 4 ', () {
         //Arrange
         Estimate estimate = Estimate(acres: 0, structures: 5);
@@ -553,10 +547,6 @@ void main() {
         expect(actual, equals(expected));
       });
 
-    test('Default Sprinklers - Structures: 10 < x < 40 ', () {
-      //Arrange
-      Estimate estimate = Estimate(acres: 0, structures: 20);
-      var expected = 40;
       test('10 < Structures < 40 -> Sprinklers = 10', () {
         //Arrange
         Estimate estimate = Estimate(acres: 0, structures: 20);
@@ -568,73 +558,34 @@ void main() {
         //Assert
         expect(actual, equals(expected));
       });
+    });
 
-    test('Default Sprinklers - Structures: x > 40 ', () {
-      //Arrange
-      Estimate estimate = Estimate(acres: 0, structures: 50);
-      var expected = 50;
-      test('Structures > 40 -> Sprinklers = 13 ', () {
-        //Arrange
-        Estimate estimate = Estimate(acres: 0, structures: 50);
-        var expected = 13;
-
-        //Act
-        var actual = estimate.defaultSprinklerKits();
-
-        //Assert
-        expect(actual, equals(expected));
+    // OnePointFive Inch Hose
+    group('defaultOnePointFiveInchHose', () {
+      test('0 when there are no structures', () {
+        Estimate estimate = Estimate(acres: 0, structures: 0);
+        expect(estimate.defaultOnePointFiveHose(), 0);
       });
-
-    });
-   
-    // OnePointFive
-
-    test('Default OnePointFive Hose - Structures: 0', () {
-      //Arrange
-      Estimate estimate = Estimate(acres: 0, structures: 0);
-      var expected = 0;
-
-      //Act
-      var actual = estimate.defaultOnePointFiveHose();
-
-      //Assert
-      expect(actual, equals(expected));
-    });
-
-    test('Default OnePointFive Hose - Structures: 0 < x < 10 ', () {
-      //Arrange
-      Estimate estimate = Estimate(acres: 0, structures: 5);
-      var expected = 3000;
-
-      //Act
-      var actual = estimate.defaultOnePointFiveHose();
-
-      //Assert
-      expect(actual, equals(expected));
-    });
-
-    test('Default OnePointFive Hose - Structures: 10 < x < 40 ', () {
-      //Arrange
-      Estimate estimate = Estimate(acres: 0, structures: 20);
-      var expected = 5000;
-
-      //Act
-      var actual = estimate.defaultOnePointFiveHose();
-
-      //Assert
-      expect(actual, equals(expected));
-    });
-
-    test('Default OnePointFive Hose - Structures: x > 40 ', () {
-      //Arrange
-      Estimate estimate = Estimate(acres: 0, structures: 50);
-      var expected = 7000;
-
-      //Act
-      var actual = estimate.defaultOnePointFiveHose();
-
-      //Assert
-      expect(actual, equals(expected));
+      test('3000 when there 0 to 10 structures', () {
+        Estimate estimate = Estimate(acres: 0, structures: 5);
+        expect(estimate.defaultOnePointFiveHose(), 3000);
+      });
+      test('5000 when there are exactly 10 structures', () {
+        Estimate estimate = Estimate(acres: 0, structures: 10);
+        expect(estimate.defaultOnePointFiveHose(), 5000);
+      });
+      test('5000 when there are 10 to 40 structures', () {
+        Estimate estimate = Estimate(acres: 0, structures: 20);
+        expect(estimate.defaultOnePointFiveHose(), 5000);
+      });
+      test('5000 when there are exactly 40 structures', () {
+        Estimate estimate = Estimate(acres: 0, structures: 40);
+        expect(estimate.defaultOnePointFiveHose(), 5000);
+      });
+      test('7000 when there are greater than 40 structures', () {
+        Estimate estimate = Estimate(acres: 0, structures: 50);
+        expect(estimate.defaultOnePointFiveHose(), 7000);
+      });
     });
 
     // One Inch Hose
